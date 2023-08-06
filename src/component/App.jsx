63abce5b07a6d8b676bb2bcfd886d70061dfc5c7
Jailan Mirkhel <100057528+iamjailan@users.noreply.github.com>
2023-08-06 16:30:12 +0430

    setup done and navbar created

---
+++ src/component/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../index.css"
+import "./pages/pages.css"
+import Navbar from "./pages/navbar";
+
+
+export default function App(){
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
---
+++ src/component/pages/navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Navbar(){
+    return (
+        <nav className="navbar">
+            <h1>Wadan Company</h1>
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>Products</li>
+                    <li>Pricing</li>
+                    <li>Lists</li>
+                    <li>Help</li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
