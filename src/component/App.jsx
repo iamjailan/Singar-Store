7c3af5f6879b56d9859772335938b9644d3ac096
Jailan Mirkhel <100057528+iamjailan@users.noreply.github.com>
2023-08-06 16:45:08 +0430

    footer added to the project

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "../index.css"
 import "./pages/pages.css"
 import Navbar from "./pages/navbar";
+import Footer from "./pages/footer";
 
 
 export default function App(){
     return (
         <>
             <Navbar />
+            <Footer />
         </>
     )
 }
---
+++ src/component/pages/footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer(){
+    return (
+        <footer className="footer">
+            <p>&copy; Wadan Group of Companies.</p>
+        </footer>
+    )
+}
