04e9aed3ea73c8b6a6acc8aceefedad07ecd0943
Jailan Mirkhel <100057528+iamjailan@users.noreply.github.com>
2023-08-08 14:47:47 +0430

    Products Page updated, the land page added, smal dashboard for user to get discount added. page  responived for mobile an table

--- src/component/pages/products/product.jsx
+++ src/component/pages/products/product.jsx
@@ -1,9 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Product(){
     return (
-        <div>
-            <h1>This is Product page</h1>
-        </div>
+        <section className="products">
+            <div className="product-land">
+                <div className="product-wel">
+                    <h1>Git 5% Cash</h1>
+                    <h1>On every single Product</h1>
+                    <p>From stylish fashion accessories to state-of-the-art gadgets, we have something for everyone. Browse through our collection and find the perfect item to enhance your lifestyle. Happy shopping!</p>
+                    <Link className="learn-btn">Learn More</Link>
+                </div>
+            </div>
+            <main className="discount">
+                <h2 className="discount-title">Save your money here!</h2>
+                <div className="discount-cards">
+                    <div className="discount-card first">
+                        <h2 className="discount-save">Save</h2>
+                        <h1 className="discount-price">$100</h1>
+                        <p className="discount-text">Buy up to 10 clothe at one time and get $100 as gift.🎉</p>
+                    </div>
+                    <div className="discount-card second">
+                        <h2 className="discount-save">Save</h2>
+                        <h1 className="discount-price">$70</h1>
+                        <p className="discount-text">Buy up to 4 adult clothes at one time and get $$70 as gift.🎉</p>
+                    </div>
+                    <div className="discount-card third">
+                        <h2 className="discount-save">Save</h2>
+                        <h1 className="discount-price">$40</h1>
+                        <p className="discount-text">Buy up to 2 couple shoes at one time and get $40 as gift.🎉</p>
+                    </div>
+                    <div className="discount-card fourth">
+                        <h2 className="discount-save">Save</h2>
+                        <h1 className="discount-price">$50</h1>
+                        <p className="discount-text">Invite 10 friend and get $50 as discount gift for each item.🎉</p>
+                    </div>
+                </div>
+            </main>
+        </section>
     )
 }
