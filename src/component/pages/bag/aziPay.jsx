1b179f7d33166e9760e29db6b248caeea2e594d2
Jailan Mirkhel <100057528+iamjailan@users.noreply.github.com>
2023-08-17 22:29:26 +0430

    Some CSS added in az-pay, QRCode added to websit

--- src/component/pages/bag/aziPay.jsx
+++ src/component/pages/bag/aziPay.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { useContext } from "react";
 import { ModeContext } from "../modeContext";
-
+import QRCode from 'qrcode.react' 
 export default function AziPay(){
     const { darkState } = useContext(ModeContext)
+    const pageUrl = window.location.href;
     return (
         <article className={darkState ? "azi-pay dark" : "azi-pay"}>
             <h1>Scan this QR code with AziPay!</h1>
             <div className="qr-code">
-
+                <QRCode value={pageUrl} />
             </div>
         </article>
     )
